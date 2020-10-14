4a05d7d66c031b1aa1106321c86d24177de172c1
bardelli5 <lucas.bardelli@hotmail.com>
2020-10-14 02:26:07 -0300

    dia-2

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,49 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false,
+}
+
+//create map
+const map = L.map('mapid', options).setView([-23.6240429,-46.5374912,17], 15);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
+.addTo(map);
+
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popAnchor: [170, 2]
+})
+
+//create and add marker
+L
+.marker([-23.6240429,-46.5374912,17], { icon })
+.addTo(map)
+
+
+/* image gallery */
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove('active');
+    }
+
+    const image = button.children[0]
+    const imageContainer = document.querySelector('.orphanage-details > img')
+
+    imageContainer.src = image.src
+
+    button.classList.add('active')
+}
